92ab9d3ed8b99b0ba96329096f3ef3da6f29a8a4
r59q <andreashenriksen@yahoo.dk>
2023-03-23 14:02:22 +0100

    Comments on lipschitz calculator

--- analysis/lipschitz.py
+++ analysis/lipschitz.py
@@ -2,12 +2,13 @@
 import numpy as np
 
 
-def lipschitz_neg_log_likelihood(feature_set: np.ndarray):
+def lipschitz_binary_neg_log_likelihood(feature_set: np.ndarray):
     """
     The Lipschitz constant, L, for maximum likelihood is
     L=(1/2n)\sum_{i=1}^n ||{x_i}||^2
 
-    Use feature set X as input to compute it's lipschitz constant
+    Use feature set X as input to compute it's lipschitz constant. 
+    Can only be used for binary classification models! todo: maybe change to allow for intervals
     """
     feature_norm_sum = 0
     for features in feature_set:
