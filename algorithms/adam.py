e71644fde1dc19b33c78e0000b62323467585e73
Mads Kongstad Laugesen <40210425+mkongstad@users.noreply.github.com>
2023-05-03 14:12:38 +0200

    adam

---
+++ algorithms/adam.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+
+class Adam:
+    def __init__(self, step_size, b1=0.94, b2=0.9879, e=10e-8):
+        if b1 > 1 or b1 < 0:
+            raise ValueError
+        if b2 > 1 or b2 < 0:
+            raise ValueError
+        self.step_size = step_size
+        self.b1 = b1
+        self.b2 = b2
+        self.m_t = 0
+        self.v_t = 0
+        self.e = e
+        self.t = 0
+
+
+    def step(self, w, derivation):
+        # https://arxiv.org/pdf/1412.6980.pdf
+        self.t += 1
+
+        grad_current = derivation(w)
+        self.m_t = self.b1 * self.m_t + (1-self.b1) * grad_current
+        self.v_t = self.b2 * self.v_t + (1-self.b2) * ( np.square(grad_current) )
+        self.mh_t = self.m_t/(1-(self.b1 ** self.t))
+        self.vh_t = self.v_t/(1-(self.b2 ** self.t))
+
+        res = w - (self.step_size * (self.mh_t/(np.sqrt(self.vh_t)+self.e)))
+        return res
