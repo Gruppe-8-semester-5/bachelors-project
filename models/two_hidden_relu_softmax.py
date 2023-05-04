ba2f3efba248c515c3c20cfe4ce2db2a073f68fa
Casper Dalgaard Nielsen <casper@dalgaardnielsen.dk>
2023-05-04 11:19:07 +0200

    Two hidden

---
+++ models/two_hidden_relu_softmax.py
@@ -0,0 +1,66 @@
+from typing import TypedDict
+import torch
+import numpy as np
+
+softmax = torch.nn.Softmax(dim = 1) 
+relu = torch.nn.ReLU()
+
+class WeightDict(TypedDict):
+    W1: torch.Tensor
+    b1: torch.Tensor
+    W2: torch.Tensor
+    b2: torch.Tensor
+
+
+def initial_params(input_dim, hidden_layer, output_dim):
+    W1 = np.random.normal(size=(input_dim, hidden_layer))
+    b1 = np.random.normal(size=(hidden_layer))
+    W2 = np.random.normal(size=(hidden_layer, output_dim))
+    b2 = np.random.normal(size=(output_dim))
+    return np.array([W1, b1, W2, b2], dtype=object)
+
+def forward(X, w: WeightDict):
+    return softmax(relu(X @ w['W1'] + w['b1']) @ w['W2'] + w['b2'])
+
+def dict_to_torch(dic):
+    return {k: torch.from_numpy(v) for k, v in dic.items()}
+
+def get_params(weights) -> WeightDict:
+    return dict_to_torch({'W1': weights[0], 'b1': weights[1], 'W2': weights[2], 'b2': weights[3]})
+    # w1 = weights[0]
+    # b1 = weights[1]
+    # w2 = weights[2]
+    # b2 = weights[3]
+    # return w1, b1, w2, b2
+
+def to_torch(*vals):
+    return map(torch.from_numpy, vals)
+
+def predict(w:np.ndarray, features: np.ndarray):
+    """The logistic regression prediction for a single point"""
+    w = get_params(w)
+    # print(softmax(forward(*to_torch(features, w, b))))
+    return torch.argmax(softmax(forward(*to_torch(features), w)), dim=1).numpy()
+    # exit()
+    # return sigmoid(w['transpose() @ features)
+
+
+def negative_log_likelihood(X, y, w):
+    pred = forward(X, w)
+    loss = torch.nn.NLLLoss()
+    return loss(pred, y.long())
+
+def gradient(X, y, weights):
+    X, y = to_torch(X, y)
+    w = get_params(weights)
+    w['W1'].requires_grad_()
+    w['b1'].requires_grad_()
+    w['W2'].requires_grad_()
+    w['b2'].requires_grad_()
+    nll = negative_log_likelihood(X, y, w)
+    nll.backward()
+    # print(w['b2'].grad.numpy().any())
+    # print(k)
+    arr = [w['W1'], w['b1'], w['W2'], w['b2']]
+    res_arr = [x.grad.numpy() for x in arr]
+    return np.array(res_arr, dtype=object)
--- run_gd_template.py
+++ run_gd_template.py
@@ -12,13 +12,16 @@ from test_runner.test_runner_file import Runner
 import models.logistic_torch as torch_logistic
 import models.logistic_regression as normal_logistic
 import models.one_hidden_softmax as cur_model
+# import models.two_hidden_relu_softmax as cur_model
+# Adam, two_hidden, 1000 iterations: 0.8255333333333333
+# Adam, one_hidden, 1000 iterations: 0.7707833333333334
 # import models.logistic_torch as cur_model
 # from models.logistic_regression import logistic_regression
 # from models.logistic_regression import gradient
 # from models.logistic_regression import predict
 
 epsilon=1.0e-2
-iterations = 100
+iterations = 1000
 one_in_k = False
 
 X, y = mnist_train_X_y(one_in_k)
@@ -33,8 +36,8 @@ if one_in_k:
 else:
     output_shape = np.amax(y) + 1
 
-# w0 = cur_model.initial_params(X.shape[1], y.shape[1])
 w0 = cur_model.initial_params(X.shape[1], output_shape)
+# w0 = cur_model.initial_params(X.shape[1], 100, output_shape)
 # w0 = cur_model.initial_params(X)
 # grad = lambda w: gradient(X, y, w)
 # List of things we want to test. Form (optimizer, params)
