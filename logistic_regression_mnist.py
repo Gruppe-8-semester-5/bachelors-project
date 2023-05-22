b2b11e1e7ce1fbc1c3ce1780ee5a6876b4c5c70c
A. Malthe Henriksen <andreashenriksen@yahoo.dk>
2023-05-22 23:25:37 +0200

    mnist tests

--- logistic_regression_mnist.py
+++ logistic_regression_mnist.py
@@ -2,6 +2,7 @@ import math
 import random
 import matplotlib.pyplot as plt
 import numpy as np
+from algorithms.accelerated_GD_adaptive import Nesterov_acceleration_adaptive
 from algorithms.adam import Adam
 from analysis.gradient_descent_result_plotting import GradientDescentResultPlotter
 from datasets.mnist.files import mnist_test_X_y, read_train_data, read_test_data, mnist_train_X_y
@@ -12,21 +13,42 @@ from algorithms.standard_GD import Standard_GD
 from algorithms.momentum_GD import Momentum
 from algorithms.accelerated_GD import Nesterov_acceleration
 from models.utility import accuracy_k_encoded
-from models import multinomial_logistic_regression, two_hidden_relu_softmax
+from models import logistic_torch, multinomial_logistic_regression, two_hidden_relu_softmax
 from test_runner.test_runner_file import Runner
 from sklearn.preprocessing import PolynomialFeatures
-np.random.seed(0)
+np.random.seed(2)
 
-X_train, y_train = mnist_train_X_y(1000)
-X_test, y_test = mnist_test_X_y(1000)
+X_train, y_train = mnist_train_X_y()
+X_test, y_test = mnist_test_X_y()
 
 
 K = 10
 
-iterations = 100000
-runner_results: list[GradientDescentResult] = [] # Our results
-input_dim = X_train.shape[1]
+iterations = 2000
+np.random.seed(123)
 w0 = two_hidden_relu_softmax.initial_params(X_train.shape[1], 50, K)
+
+startw = logistic_torch.initial_params(X_train)
+test_set = {
+    'w0': startw,
+    'GD_params': {'w0': startw, 'start_alpha':0.01},
+    # 'GD_params': {'L': [0.01], 'w0': w0},
+    'alg': [Standard_GD],
+    'model': logistic_torch,
+    'max_iter': iterations,
+    'data_set': (X_train, y_train),
+    'test_set': (X_test, y_test),
+    'epsilon':0,
+    'auto_stop': False,
+    'batch': None
+}
+runner = Runner(dic = test_set)
+logistic_regression_mnist: GradientDescentResult = runner.get_result()[0]
+
+
+print("Logistic regression accuracy", logistic_regression_mnist.get_best_accuracy())
+GradientDescentResultPlotter([logistic_regression_mnist]).plot_accuracies_over_time().plot()
+
 test_set = {
     'w0': w0,
     'GD_params': {'step_size': [0.01]},
@@ -41,26 +63,6 @@ test_set = {
     'batch': None
 }
 runner = Runner(dic = test_set)
-std_gd_result: GradientDescentResult = runner.get_result()[0]
-runner_results.append(std_gd_result)
-
-test_set = {
-    'w0': w0[0],
-    'GD_params': {'step_size': [0.2]},
-    # 'GD_params': {'L': [0.01], 'w0': w0},
-    'alg': [Standard_GD],
-    'model': multinomial_logistic_regression,
-    'max_iter': iterations,
-    'data_set': (X_train, y_train),
-    'test_set': (X_test, y_test),
-    'epsilon':0,
-    'auto_stop': False,
-    'batch': None
-}
-runner = Runner(dic = test_set)
-runner_results.append(runner.get_result()[0])
-
-
-#print(runner_results[0].accuracies)
-
-GradientDescentResultPlotter([runner_results[1]]).plot_accuracies_over_time().plot()
+nn_result: GradientDescentResult = runner.get_result()[0]
+print("Logistic regression accuracy", logistic_regression_mnist.get_best_accuracy())
+GradientDescentResultPlotter([nn_result]).plot_accuracies_over_time().plot_function(lambda x: logistic_regression_mnist.get_accuracy_over_time()[x]).legend_placed("center right").with_result_labelled(["Neural network"]).with_functions_labelled(["Logistic regression - NAG"]).plot()
--- standard_gradient_descent_on_wine_convergence_guarantee_likelihood.py
+++ standard_gradient_descent_on_wine_convergence_guarantee_likelihood.py
@@ -2,92 +2,76 @@ import matplotlib.pyplot as plt
 import numpy as np
 from algorithms.accelerated_GD import Nesterov_acceleration
 from analysis.gradient_descent_result_plotting import GradientDescentResultPlotter
-from datasets.winequality.files import read_wine_data
+from datasets.winequality.files import read_wine_data, wine_X_y
 from analysis.lipschitz import lipschitz_binary_neg_log_likelihood
 from datasets.winequality.wine import Wine
-from models.logistic_regression import gradient, negative_log_likelihood, predict
+from models import logistic_torch
 from algorithms import GradientDescentResult, gradient_descent_template, standard_GD
 from algorithms.standard_GD import Standard_GD
 from algorithms.momentum_GD import Momentum
-from models.utility import accuracy
+from models.utility import accuracy, to_torch
 from analysis.utility import dump_array_to_csv, euclid_distance
+from test_runner.test_runner_file import Runner
 
-# Construct np array of features
-def color_to_label(color):
-    if color == "white":
-        return -1.
-    return 1.
+# Features and labels
+X, y = wine_X_y()
 
-dataset = read_wine_data()
-wines: list[Wine] = list(map(lambda d: Wine(d), dataset))
-feature_list = list(map(lambda wine: wine.get_feature_vector(), wines))
-feature_array = np.array(feature_list)
-color_label_list = list(map(lambda wine: color_to_label(wine.get_color()), wines))
-color_label_array = np.array(color_label_list)
-lipschitz = lipschitz_binary_neg_log_likelihood(feature_array, color_label_array)
-print(f"lipschitz = {lipschitz}")
-n = len(dataset)
-print(f"n = {n}")
+# Lipschitz bound
+L = lipschitz_binary_neg_log_likelihood(X, y)
 
+# Starting point
+np.random.seed(123) # Set seed for replication purposes
+start_gradient = logistic_torch.initial_params(X)
 
+test_set = {
+    'w0': start_gradient,
+    'GD_params': {'step_size': 1/L},
+    'alg': [Standard_GD],
+    'model': logistic_torch,
+    'max_iter': 100000,
+    'data_set': (X, y),
+    'epsilon':1.0e-2,
+    'auto_stop': False,
+    'batch': None
+}
+runner = Runner(dic = test_set)
+descent_result_lipchitz_best = runner.get_result()[0]
 
-example_wine = wines[0]
-# print("feature example", example_wine.get_feature_vector())
-# print("Label example", color_to_label(example_wine.get_color())) 
-
-feature_size = example_wine.get_feature_vector().size
-
-def make_predictions(weights):
-    predictions = []
-    for wine in wines:
-        predictions.append(predict(weights, wine.get_feature_vector()))
-    return predictions
-
-start_gradient = np.zeros(feature_size)
+# Run GD to test convergence
 iterations = 100
+test_set = {
+    'w0': start_gradient,
+    'GD_params': {'step_size': 1/L},
+    'alg': [Standard_GD],
+    'model': logistic_torch,
+    'max_iter': iterations,
+    'data_set': (X, y),
+    'epsilon':1.0e-2,
+    'auto_stop': False,
+    'batch': None
+}
+runner = Runner(dic = test_set)
+descent_result_lipchitz = runner.get_result()[0]
 
-descent_result_lipchitz_with_w_star: GradientDescentResult = gradient_descent_template.find_minima(
-    start_gradient, 
-    Standard_GD(1/(lipschitz)),
-    lambda w: gradient(feature_array, color_label_array, w), 
-    max_iter=100000,
-    epsilon=1.0e-2,
-    accuracy=(lambda w: accuracy(color_label_array, make_predictions(w))))
-
-descent_result_lipchitz: GradientDescentResult = gradient_descent_template.find_minima(
-    start_gradient, 
-    Standard_GD(1/(lipschitz)),
-    lambda w: gradient(feature_array, color_label_array, w), 
-    max_iter=iterations,
-    epsilon=1.0e-2,
-    accuracy=(lambda w: accuracy(color_label_array, make_predictions(w))))
-
+# Collect weights and map to NLL values
 weights = descent_result_lipchitz.get_weights_over_time()
-best_nll = negative_log_likelihood(feature_array, color_label_array, weights[0])
+likelihood_values = [logistic_torch.negative_log_likelihood(*to_torch(X, y, weight)) for weight in weights]
+# Find w* (approximate)
+w_star = descent_result_lipchitz_best.get_weights_over_time()[-1]
+f_w_star = logistic_torch.negative_log_likelihood(*to_torch(X, y, w_star))
 
-likelihood_values = [negative_log_likelihood(feature_array, color_label_array, weight) for weight in weights]
-
-
-#likelihood_values = []
-#for weight in weights:
-#    current_nll = negative_log_likelihood(feature_array, color_label_array, weight)
-#    if current_nll > best_nll:
-#        best_nll = current_nll
-#    likelihood_values.append(best_nll)
-
-
-f_w_star = negative_log_likelihood(feature_array, color_label_array, descent_result_lipchitz_with_w_star.get_best_weights_over_time()[len(descent_result_lipchitz_with_w_star.get_best_weights_over_time())-1])
-
+# Compute f(w) - f(w*)
 diff_to_w_star = [np.abs(nll_val - f_w_star) for nll_val in likelihood_values]
-best_diff_to_w_star = diff_to_w_star[0]
-distances_to_w_star_only_best = [ ]
 
+# Discard any 'backwards' steps to only keep lower bound values
+best_diff_to_w_star = diff_to_w_star[0]
+diff_to_w_star_only_best = [ ]
 for current_diff_to_w_star in diff_to_w_star:
     best_diff_to_w_star = min(best_diff_to_w_star, current_diff_to_w_star)
-    distances_to_w_star_only_best.append(best_diff_to_w_star)
+    diff_to_w_star_only_best.append(best_diff_to_w_star)
 
-convergence_rate_multiplier = 8000 # Labeled C in plot
-GradientDescentResultPlotter([descent_result_lipchitz]).plot_function(lambda x: distances_to_w_star_only_best[x]).legend_placed("upper right").with_functions_labelled(["f(w_x) - f(w*)", "C/x"]).with_x_values(range(0,iterations)).plot()
+convergence_rate_multiplier = 1.5 # Labeled C in plot
+GradientDescentResultPlotter([descent_result_lipchitz]).plot_function(lambda x: convergence_rate_multiplier/x if x!=0 else convergence_rate_multiplier).plot_function(lambda x: diff_to_w_star[x]).legend_placed("upper right").with_functions_labelled(["C/k", "f(w_k) - f(w*)"]).with_x_values(range(0,iterations)).plot()
 
 #GradientDescentResultPlotter([descent_result_lipchitz]).plot_function(lambda x :-likelihood_values[x] if -likelihood_values[x] > 0 else 0).plot_function(lambda x: convergence_rate_multiplier/x if x != 0 else convergence_rate_multiplier).legend_placed("upper right").with_functions_labelled(["f(w)", "2.750.000/x"]).with_x_values(range(0,iterations)).plot()
 
