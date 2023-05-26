15520e3f9c5a96805ad85cb4923be0f6b042b3c6
Casper Dalgaard Nielsen <casper@dalgaardnielsen.dk>
2023-05-26 21:11:38 +0200

    Made 'graphs' of different parameters for momentum and nesterov

--- convex_convergence_rate_nesterov.py
+++ convex_convergence_rate_nesterov.py
@@ -1,6 +1,6 @@
 from matplotlib import pyplot as plt
 import numpy as np
-from algorithms.heavy_ball import Heavy_ball
+from algorithms.accelerated_GD_adaptive import Nesterov_acceleration_adaptive
 from algorithms.adam import Adam
 from datasets.winequality.files import wine_X_y
 from analysis.lipschitz import lipschitz_binary_neg_log_likelihood
@@ -30,14 +30,12 @@ w0_list = [w0] + [logistic.initial_params(X) for _ in range(100-1)]
 L = lipschitz_binary_neg_log_likelihood(X, y)
 used = 1 / L
 constants_normal = [0.2, 0.4, 0.6, 0.8, 1, 1.2, 1.4, 1.6, 1.8, 2]
-betas = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-# constants_normal = [0.2, 2]
-# betas = [0.1, 0.9]
+alphas = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 step_sizes_normal = [used * x for x in constants_normal]
 test_set = {
     "w0": w0_list,
-    "GD_params": {"step_size": step_sizes_normal, 'beta': betas},
-    "alg": [Heavy_ball],
+    "GD_params": {"step_size": step_sizes_normal, 'start_alpha': alphas},
+    "alg": [Nesterov_acceleration_adaptive],
     "model": logistic,
     "max_iter": iterations,
     "data_set": (X, y),
@@ -45,34 +43,22 @@ test_set = {
     "batch": None,
 }
 
-best_ = {
-    "w0": w0,
-    "GD_params": {"step_size": 1 / L},
-    "alg": [Adam],
-    "model": logistic,
-    "max_iter": 10000,
-    "data_set": (X, y),
-    "epsilon": epsilon,
-    "batch": None,
-}
-
 runner = Runner(dic=test_set)
 # results_desc = runner.get_res_and_description()
-# results_normal = runner.get_result()
+results_normal = runner.get_result()
 
-runner_ = Runner(dic=best_)
-w_star = runner_.get_result()[0].get_weights_over_time()[-1]
-smallest_loss = logistic.negative_log_likelihood(*to_torch(X, y, w_star))
 
-x_values = [i for i in range(1, iterations + 1)]
+
+# We get an accuracy, at early stop (using L2 with L2_const=50 and standard parameters without 1/L2_const multiplied):
+# 0.8591657688163767
 
 # https://matplotlib.org/stable/gallery/images_contours_and_fields/image_annotated_heatmap.html
-plot_this = np.zeros(shape=(len(constants_normal), len(betas)))
+plot_this = np.zeros(shape=(len(constants_normal), len(alphas)))
 for i in range(len(constants_normal)):
-    for j in range(len(betas)):
-        a = step_sizes_normal[i]
-        b = betas[j]
-        results = runner.get_result(GD_params={"step_size": a, 'beta': b})
+    for j in range(len(alphas)):
+        step_size = step_sizes_normal[i]
+        a = alphas[j]
+        results = runner.get_result(GD_params={"start_alpha": a, 'step_size': step_size})
         n = len(results)
         mean = 0
         for k in range(n):
@@ -83,8 +69,10 @@ fig, ax = plt.subplots()
 im = ax.imshow(plot_this)
 
 # Show all ticks and label them with the respective list entries
-ax.set_xticks(np.arange(len(betas)), labels=betas)
+ax.set_xticks(np.arange(len(alphas)), labels=alphas)
 ax.set_yticks(np.arange(len(constants_normal)), labels=np.char.add(np.array(constants_normal).astype('str'), "/L"))
+plt.xlabel("\u03B1")
+plt.ylabel("step size")
 
 # Rotate the tick labels and set their alignment.
 plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
@@ -96,68 +84,32 @@ plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
 #         text = ax.text(j, i, plot_this[i, j],
 #                        ha="center", va="center", color="w")
 
-ax.set_title("Mean loss alpha/beta")
+ax.set_title("Mean loss alpha/step size")
 fig.tight_layout()
 cbar = ax.figure.colorbar(im, ax=ax)
 # cbar.ax.set_ylabel('some text', rotation=-90, va="bottom")
 # plt.show()
-save('momentum_heat_mean_all')
-
-
-plot_this = np.zeros(shape=(len(constants_normal), len(betas)))
-for i in range(len(constants_normal)):
-    for j in range(len(betas)):
-        a = step_sizes_normal[i]
-        b = betas[j]
-        results = runner.get_result(GD_params={"step_size": a, 'beta': b})
-        n = len(results)
-        mean = 0
-        for k in range(n):
-            mean += np.mean(results[k].get_losses_over_time()[:10])
-        mean /= n
-        plot_this[i][j] = mean
-fig, ax = plt.subplots()
-im = ax.imshow(plot_this)
-
-# Show all ticks and label them with the respective list entries
-ax.set_xticks(np.arange(len(betas)), labels=betas)
-ax.set_yticks(np.arange(len(constants_normal)), labels=np.char.add(np.array(constants_normal).astype('str'), "/L"))
-
-# Rotate the tick labels and set their alignment.
-plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-         rotation_mode="anchor")
-
-# Loop over data dimensions and create text annotations.
-# for i in range(len(step_sizes_normal)):
-#     for j in range(len(betas)):
-#         text = ax.text(j, i, plot_this[i, j],
-#                        ha="center", va="center", color="w")
-
-ax.set_title("Mean over first 10 losses")
-fig.tight_layout()
-cbar = ax.figure.colorbar(im, ax=ax)
-# cbar.ax.set_ylabel('some text', rotation=-90, va="bottom")
-save('momentum_heat_first_10')
 # plt.show()
-exit()
+save('nesterov_heat_mean_all')
 
-# plot_this = np.zeros(shape=(len(constants_normal), len(betas)))
+
+# plot_this = np.zeros(shape=(len(constants_normal), len(alphas)))
 # for i in range(len(constants_normal)):
-#     for j in range(len(betas)):
-#         a = step_sizes_normal[i]
-#         b = betas[j]
-#         results = runner.get_result(GD_params={"step_size": a, 'beta': b})
+#     for j in range(len(alphas)):
+#         step_size = step_sizes_normal[i]
+#         a = alphas[j]
+#         results = runner.get_result(GD_params={"start_alpha": a, 'step_size': step_size})
 #         n = len(results)
 #         mean = 0
 #         for k in range(n):
-#             mean += results[k].get_losses_over_time()[-1]
+#             mean += np.mean(results[k].get_losses_over_time()[:10])
 #         mean /= n
 #         plot_this[i][j] = mean
 # fig, ax = plt.subplots()
 # im = ax.imshow(plot_this)
 
 # # Show all ticks and label them with the respective list entries
-# ax.set_xticks(np.arange(len(betas)), labels=betas)
+# ax.set_xticks(np.arange(len(alphas)), labels=alphas)
 # ax.set_yticks(np.arange(len(constants_normal)), labels=np.char.add(np.array(constants_normal).astype('str'), "/L"))
 
 # # Rotate the tick labels and set their alignment.
@@ -170,8 +122,48 @@ exit()
 # #         text = ax.text(j, i, plot_this[i, j],
 # #                        ha="center", va="center", color="w")
 
-# ax.set_title("Last element")
+# ax.set_title("Mean over first 10 losses")
 # fig.tight_layout()
 # cbar = ax.figure.colorbar(im, ax=ax)
 # # cbar.ax.set_ylabel('some text', rotation=-90, va="bottom")
+# # save('momentum_heat_first_10')
 # plt.show()
+
+
+plot_this = np.zeros(shape=(len(constants_normal), len(alphas)))
+for i in range(len(constants_normal)):
+    for j in range(len(alphas)):
+        step_size = step_sizes_normal[i]
+        alpha = alphas[j]
+        results = runner.get_result(GD_params={"start_alpha": alpha, 'step_size': step_size})
+        n = len(results)
+        mean = 0
+        for k in range(n):
+            mean += results[k].get_losses_over_time()[-1]
+        mean /= n
+        plot_this[i][j] = mean
+fig, ax = plt.subplots()
+im = ax.imshow(plot_this)
+
+# Show all ticks and label them with the respective list entries
+ax.set_xticks(np.arange(len(alphas)), labels=alphas)
+ax.set_yticks(np.arange(len(constants_normal)), labels=np.char.add(np.array(constants_normal).astype('str'), "/L"))
+plt.xlabel("\u03B1")
+plt.ylabel("step size")
+
+# Rotate the tick labels and set their alignment.
+plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+         rotation_mode="anchor")
+
+# Loop over data dimensions and create text annotations.
+# for i in range(len(step_sizes_normal)):
+#     for j in range(len(betas)):
+#         text = ax.text(j, i, plot_this[i, j],
+#                        ha="center", va="center", color="w")
+
+ax.set_title("Last element")
+fig.tight_layout()
+cbar = ax.figure.colorbar(im, ax=ax)
+# cbar.ax.set_ylabel('some text', rotation=-90, va="bottom")
+# plt.show()
+save('nesterov_heat_last')
